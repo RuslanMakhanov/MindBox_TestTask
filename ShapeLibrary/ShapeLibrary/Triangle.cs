fa8b68d1e192773fdbf0ee7dcaeb9b701fff9b6e
RuslanNnNnNn <95371964+RuslanMakhanov@users.noreply.github.com>
2024-10-11 21:05:47 +0500

    Created interfaces and classes. Added  UnitTEst

---
+++ ShapeLibrary/ShapeLibrary/Triangle.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeLibrary
+{
+    public class Triangle : IShape
+    {
+        public double A { get; }
+        public double B { get; }
+        public double C { get; }
+
+        public Triangle(double a, double b, double c)
+        {
+            A = a;
+            B = b;
+            C = c;
+
+            if (!IsValidTriangle())
+                throw new ArgumentException("Некорректные длины сторон для треугольника.");
+        }
+
+        public double CalculateArea()
+        {
+            // Используем формулу для расчета площади треугольника
+            double s = (A + B + C) / 2;
+            return Math.Sqrt(s * (s - A) * (s - B) * (s - C));
+        }
+
+        public bool IsRightTriangle()
+        {
+            // Проверка на прямоугольный треугольник
+            var sides = new[] { A, B, C };
+            Array.Sort(sides); // Сортируем для упрощения проверки
+            return Math.Abs(sides[2] * sides[2] - (sides[0] * sides[0] + sides[1] * sides[1])) < 0.0001;
+        }
+
+        private bool IsValidTriangle()
+        {
+            return A + B > C && A + C > B && B + C > A;
+        }
+    }
+
+}
